81db6c6634657a4e58cf1261c808e59afb78f11a
Diego Mondego <diego@mangobits.com>
2020-03-16 16:49:04 -0300

    rermoção da dependencia do MSKNotification.

--- src/main/java/com/mangobits/startupkit/authkey/UserAuthKeyService.java
+++ src/main/java/com/mangobits/startupkit/authkey/UserAuthKeyService.java
@@ -4,10 +4,8 @@ import javax.ejb.Local;
 
 @Local
 public interface UserAuthKeyService {
-
 	
 	UserAuthKey createKey(String idUser, UserAuthKeyTypeEnum type) throws Exception;
-	
-	
+
 	Boolean validateKey(UserAuthKey key) throws Exception;
 }
